715b8f78dab1b2fee8cb9b67837ce6a5ea947dad
prostodasha62@gmail.com <prostodasha62@gmail.com>
2024-12-15 03:17:48 +0500

    Add project files.

---
+++ Tyuiu.KorobeinikovaDD.Sprint7.Project.V5/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KorobeinikovaDD.Sprint7.Project.V5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
